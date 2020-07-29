466bc847497a7c8920c5a24a531aa52a2e2dbcf4
ines-ux <65893945+ines-ux@users.noreply.github.com>
2020-07-29 21:28:34 +0200

    mercredi 29 Juillet

--- src/App.js
+++ src/App.js
@@ -2,25 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			name: '',
+			capitale: '',
+			flag: '',
+			population: 0,
+			region: ''
+		};
+	}
+
+	componentDidMount() {
+		let url = 'https://restcountries.eu/rest/v2/name/france';
+		fetch(url).then((res) => res.json()).then((json) => {
+			console.log('tester la requete json ', json);
+			console.log('chercher name', json[0].name);
+			this.setState({
+				name: json[0].name,
+				capitale: json[0].capital,
+				flag: json[0].flag,
+				population: json[0].population,
+				region: json[0].region
+			});
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<h1> Selector Country </h1>
+				<button>France</button>
+				<button>Brazil</button>
+				<button>Croatia</button>
+				<div>
+					<img src={this.state.flag} alt="" />
+					<h3>Country : {this.state.name} </h3>
+					<h4>Capitale : {this.state.capitale} </h4>
+					<p>Region : {this.state.region} </p>
+					<p>Population : {this.state.population} </p>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
